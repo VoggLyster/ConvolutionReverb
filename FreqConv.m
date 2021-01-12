cfc30b6bed594d6e50c42df1eb40c4d477322000
Søren Vøgg Lyster <soren@vogglyster.dk>
2021-01-12 16:24:53 +0100

    Save input audio after FFT

--- FreqConv.m
+++ FreqConv.m
@@ -7,7 +7,8 @@ classdef FreqConv < audioPlugin
         input_buffer;
         output_buffer;
         overlap_buffer;
-        delay_buffer;
+        delay_fft_buffer_left;
+        delay_fft_buffer_right;
         IR_frames_left;
         IR_frames_right;
         n_IR_frames = 0;
@@ -15,7 +16,7 @@ classdef FreqConv < audioPlugin
         channels = 2;
         buffer_size = 512;
         write_head = 1;
-        NFFT = 0;
+        NFFT = 1024;
     end
     properties (Constant)
         PluginInterface = audioPluginInterface( ...
@@ -26,75 +27,75 @@ classdef FreqConv < audioPlugin
     end    
     methods
         function plugin = FreqConv
-           plugin.input_buffer = dsp.AsyncBuffer;
-           setup(plugin.input_buffer,[1,1]);
-           plugin.output_buffer = dsp.AsyncBuffer;
-           setup(plugin.output_buffer,[1,1]);
-           plugin.delay_buffer = zeros(plugin.delay_buffer_size, plugin.channels);
-           plugin.overlap_buffer = zeros(plugin.buffer_size, plugin.channels);
-           plugin.IR_frames_left = [];
-           plugin.IR_frames_right = [];
+            plugin.input_buffer = dsp.AsyncBuffer;
+            setup(plugin.input_buffer,[1,1]);
+            plugin.output_buffer = dsp.AsyncBuffer;
+            setup(plugin.output_buffer,[1,1]);
+            plugin.overlap_buffer = zeros(plugin.buffer_size, plugin.channels);
+            plugin.IR_frames_left = [];
+            plugin.IR_frames_right = [];
+            plugin.delay_fft_buffer_left = zeros(1024, 1000);
+            plugin.delay_fft_buffer_right = zeros(1024, 1000);
         end
         function out = process(p, in)
-           write(p.input_buffer, in);
-           while p.input_buffer.NumUnreadSamples >= p.buffer_size
-               w = p.write_head;
-               p.delay_buffer(w:w+p.buffer_size-1,:) = read(p.input_buffer, p.buffer_size);
-               w = w + p.buffer_size;
-               if w > p.delay_buffer_size
-                  w = 1;
-               end
-               p.write_head = w;
-               overlap = zeros(p.buffer_size,2);
-               if w < p.buffer_size
-                   r = w - p.buffer_size + p.delay_buffer_size;
-               else
-                   r = w - p.buffer_size;
-               end
+            write(p.input_buffer, in);
+            while p.input_buffer.NumUnreadSamples >= p.buffer_size
+                input = read(p.input_buffer, p.buffer_size);
+                w = p.write_head;
+                p.delay_fft_buffer_left(:, w) = fft(input(:,1), p.NFFT);
+                p.delay_fft_buffer_right(:, w) = fft(input(:,2), p.NFFT);
+                w = w + 1;
+                if w > 1000
+                    w = 1;
+                end
+                p.write_head = w;
+                overlap = zeros(p.buffer_size,2);
+                r = w;
 
-               % Dry signal%
-               rev = p.delay_buffer(r:r+p.buffer_size-1,:) * (1-p.mix/100);
+                % Dry signal%
+                rev = input * (1-p.mix/100);
 
-               % Wet signal
-               rev = rev + p.overlap_buffer(1:p.buffer_size,:) * p.mix/100;
-               n_frames = min(p.n_IR_frames, p.max_frames);
-               for frame = 1:n_frames
-                      x = p.delay_buffer(r:r+p.buffer_size-1,:);
-                      IR_frame_real = real(p.IR_frames_left(1+(frame-1)*p.NFFT:frame*p.NFFT,1));
-                      IR_frame_imag = imag(p.IR_frames_left(1+(frame-1)*p.NFFT:frame*p.NFFT,1));
-                      res_left = FreqConvolute(x(:,1), IR_frame_real, IR_frame_imag, p.NFFT);
-                      res_right = FreqConvolute(x(:,2), IR_frame_real, IR_frame_imag, p.NFFT);
-                      rev(:,1) = rev(:,1) + res_left(1:p.buffer_size) / n_frames * p.mix/100;
-                      rev(:,2) = rev(:,2) + res_right(1:p.buffer_size) / n_frames * p.mix/100;
-                      overlap(:,1) = overlap(:,1) + res_left(p.buffer_size+1:p.buffer_size*2) / n_frames;
-                      overlap(:,2) = overlap(:,2) + res_right(p.buffer_size+1:p.buffer_size*2) / n_frames;
-                  if r == 1
-                    r = p.delay_buffer_size - p.buffer_size;
-                  else
-                    r = r - p.buffer_size;
-                  end
-               end
-               write(p.output_buffer,rev(:,1:2));
-               p.overlap_buffer(1:p.buffer_size,:) = overlap(1:end,:);  
-           end
-           if p.output_buffer.NumUnreadSamples >= size(in,1)
-               out = read(p.output_buffer,size(in,1));
-           else
-               out = zeros(size(in,1),2);
-           end
+                % Wet signal
+                rev = rev + p.overlap_buffer(1:p.buffer_size,:) * p.mix/100;
+                n_frames = min(p.n_IR_frames, p.max_frames);
+                for frame = 1:n_frames
+                    in_left = p.delay_fft_buffer_left(:,r);
+                    in_right = p.delay_fft_buffer_right(:,r);
+                    IR_frame_real = real(p.IR_frames_left(1+(frame-1)*p.NFFT:frame*p.NFFT,1));
+                    IR_frame_imag = imag(p.IR_frames_left(1+(frame-1)*p.NFFT:frame*p.NFFT,1));
+                    res_left = FreqConvolute(in_left, IR_frame_real, IR_frame_imag, p.NFFT);
+                    res_right = FreqConvolute(in_right, IR_frame_real, IR_frame_imag, p.NFFT);
+                    rev(:,1) = rev(:,1) + res_left(1:p.buffer_size) / n_frames * p.mix/100;
+                    rev(:,2) = rev(:,2) + res_right(1:p.buffer_size) / n_frames * p.mix/100;
+                    overlap(:,1) = overlap(:,1) + res_left(p.buffer_size+1:p.buffer_size*2) / n_frames;
+                    overlap(:,2) = overlap(:,2) + res_right(p.buffer_size+1:p.buffer_size*2) / n_frames;
+                    if r == 1
+                        r = 1000;
+                    else
+                        r = r - 1;
+                    end
+                end
+                write(p.output_buffer,rev(:,1:2));
+                p.overlap_buffer(1:p.buffer_size,:) = overlap(1:end,:);  
+            end
+            if p.output_buffer.NumUnreadSamples >= size(in,1)
+                out = read(p.output_buffer,size(in,1));
+            else
+                out = zeros(size(in,1),2);
+            end
         end
         
         function reset(p)
             IR_mat = coder.load("IR.mat"); 
             h = IR_mat.h_new;
             IR = RemoveTailBelowThreshold(h, -60);
-            p.NFFT = 2^nextpow2(p.buffer_size + 1);
             p.write_head = 1;
             [IR_frames_real, IR_frames_imag, ~] = GetUnisonPartitionedIRFrames(IR, p.NFFT, p.buffer_size);
             p.IR_frames_left = complex(IR_frames_real, IR_frames_imag);
             [IR_frames_real, IR_frames_imag, p.n_IR_frames] = GetUnisonPartitionedIRFrames(IR, p.NFFT, p.buffer_size);
             p.IR_frames_right = complex(IR_frames_real, IR_frames_imag);
-            p.delay_buffer = zeros(p.delay_buffer_size, p.channels);
+            p.delay_fft_buffer_left = zeros(1024, 1000);
+            p.delay_fft_buffer_right = zeros(1024, 1000);
             p.overlap_buffer = zeros(p.buffer_size, p.channels);
         end
     end
--- FreqConvolute.m
+++ FreqConvolute.m
@@ -1,12 +1,8 @@
 function [out, out_size] = FreqConvolute(input, ir_frame_real, ir_frame_imag, fft_frame_size)
     ir_frame = complex(ir_frame_real,ir_frame_imag);
-    input_size = numel(input);
-    input = [input; zeros(fft_frame_size-input_size,1)];
     Y = ir_frame;
     NFFT = fft_frame_size;
-    
-    X = fft(input, NFFT);
-    X_conv = X.*Y;
+    X_conv = input.*Y;
     out = real(ifft(X_conv,NFFT));
     out_size = size(out,1);
 end
